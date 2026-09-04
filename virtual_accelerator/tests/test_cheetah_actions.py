49413bf46e32be2869264f14961bb0330be45202
Ryan Roussel <rroussel@slac.stanford.edu>
2026-09-04 14:11:49 -0500

    linting

--- virtual_accelerator/tests/test_cheetah_actions.py
+++ virtual_accelerator/tests/test_cheetah_actions.py
@@ -77,7 +77,7 @@ class TestCheetahActions:
                     name="Q1",
                     length=torch.tensor(0.5),
                     k1=torch.tensor(1.0),
-                    metadata={"alias": "QUAD:IN20:511"}
+                    metadata={"alias": "QUAD:IN20:511"},
                 ),
                 Drift(name="D1", length=torch.tensor(1.0)),
             ]
